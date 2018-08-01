a0a9e5ed0f81f76b2808bc2229911738a4dbb41f
Haley Someone <hbarlar1@gmail.com>
2018-08-01 23:28:17 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |something, something_data|
-    something_data.each do |language, language_data|
-      language_data.each do |key, value|
+  languages.each do |styles, hashes|
+    hashes.each do |language, language_data|
+      language_data.each do |type, type_value|
         binding.pry 
       end
     end
