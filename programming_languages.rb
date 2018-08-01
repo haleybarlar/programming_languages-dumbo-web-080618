fd8ffcecf719b31df1eda7bcec4c1668ee6ae003
Haley Someone <hbarlar1@gmail.com>
2018-08-01 23:25:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,8 @@ def reformat_languages(languages)
   languages.each do |something, something_data|
     something_data.each do |language, language_data|
       language_data.each do |key, value|
-        
-
+        binding.pry 
+      end
+    end
+  end
 end
