1504682e9cb9efd4a84980e47abb49578b188d3f
Haley Someone <hbarlar1@gmail.com>
2018-08-01 23:30:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,7 @@ def reformat_languages(languages)
         if new_hash.include?(language)
           new_hash[language][:style] << styles
         else
-          new_hash[language] = {:type => str, 
+          new_hash[language] = {:type => str, :style => [style]}
       end
     end
   end
